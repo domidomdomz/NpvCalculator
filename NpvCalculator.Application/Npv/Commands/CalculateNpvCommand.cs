0f828816b25fca3f4c8b13b36bfb7ec2ce522177
domidomdomz <domz.ciriaco@gmail.com>
2025-01-30 23:06:08 +0800

    - Create CalculateNpvCommand -- a record of calculation request and decimal response - Create Handler

---
+++ NpvCalculator.Application/Npv/Commands/CalculateNpvCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace NpvCalculator.Application.Npv.Commands
+{
+    public record CalculateNpvCommand(
+        decimal InitialInvestment, 
+        decimal DiscountRateIncrement, 
+        List<decimal> CashFlows) : IRequest<decimal>;
+}
---
+++ NpvCalculator.Application/Npv/Commands/CalculateNpvCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace NpvCalculator.Application.Npv.Commands
+{
+    public class CalculateNpvCommandHandler : IRequestHandler<CalculateNpvCommand, decimal>
+    {
+        public CalculateNpvCommandHandler() { }
+
+        public Task<decimal> Handle(CalculateNpvCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
