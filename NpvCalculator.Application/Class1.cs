b480ebe2b88e6eaf44ad0f501b88284c2539fda9
domidomdomz <domz.ciriaco@gmail.com>
2025-01-31 01:53:21 +0800

    Create Application Dependency Injection

--- NpvCalculator.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NpvCalculator.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ NpvCalculator.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
+namespace NpvCalculator.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            return services;
+        }
+    }
+}
