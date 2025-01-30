38aacb14162f484891b5bef1b4304f090c34b81a
domidomdomz <domz.ciriaco@gmail.com>
2025-01-30 22:49:55 +0800

    Create Entities

---
+++ NpvCalculator.Core/Entities/CashFlow.cs
@@ -0,0 +1,11 @@
+﻿using NpvCalculator.Core.Common;
+
+namespace NpvCalculator.Core.Entities
+{
+    public class CashFlow : BaseEntity
+    {
+        public decimal Amount { get; set; }
+        public int Period { get; set; }
+        public int NpvCalculationId { get; set; }
+    }
+}
---
+++ NpvCalculator.Core/Entities/NpvCalculation.cs
@@ -0,0 +1,13 @@
+﻿using NpvCalculator.Core.Common;
+
+namespace NpvCalculator.Core.Entities
+{
+    public class NpvCalculation : BaseEntity
+    {
+        public decimal InitialInvestment { get; set; }
+        public decimal DiscountRate { get; set; }
+        public decimal Result { get; set; }
+        public DateTime CalculatedAt { get; set; } = DateTime.UtcNow;
+        public List<CashFlow> CashFlows { get; set; } = new();
+    }
+}
