1663baf42d33deb8f9c5486adf69d0d087994b67
domidomdomz <domz.ciriaco@gmail.com>
2025-01-31 01:52:54 +0800

    Create NpvController

---
+++ NpvCalculator.Api/Controllers/NpvController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NpvCalculator.Application.Npv.Commands;
+using NpvCalculator.Application.Npv.Queries;
+using NpvCalculator.Core.Entities;
+
+namespace NpvCalculator.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NpvController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public NpvController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<decimal>> Calculate([FromBody] CalculateNpvCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpGet("history")]
+        public async Task<ActionResult<List<NpvCalculation>>> GetHistory()
+        {
+            var query = new GetCalculationHistoryQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
