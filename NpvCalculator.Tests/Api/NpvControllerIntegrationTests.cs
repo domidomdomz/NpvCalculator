1a4be63ab63a05875cab6a0d8aeda6f36a638683
domidomdomz <domz.ciriaco@gmail.com>
2025-01-31 12:08:11 +0800

    Re-create Tests project to xUnit

---
+++ NpvCalculator.Tests/Api/NpvControllerIntegrationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NpvCalculator.Api;
+
+namespace NpvCalculator.Tests.Api
+{
+    public class NpvControllerIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        public NpvControllerIntegrationTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory.WithWebHostBuilder(builder => { });
+        }
+
+        private readonly WebApplicationFactory<Program> _factory;
+
+        [Fact]
+        public async Task Calculate_ReturnsValidNpvResult()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var command = new
+            {
+                InitialInvestment = 1000,
+                DiscountRate = 0.1m,
+                CashFlows = new List<decimal> { 500, 500, 500 }
+            };
+
+            // Act
+            var response = await client.PostAsJsonAsync("/api/npv", command);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadFromJsonAsync<decimal>();
+            Assert.InRange(result, 243.42m, 243.43m);
+        }
+    }
+}
--- NpvCalculator.Tests/Application/NpvCalculationTests.cs
+++ NpvCalculator.Tests/Application/NpvCalculationTests.cs
@@ -1,24 +1,24 @@
 using NpvCalculator.Application.Services;
 
-namespace NpvCalculator.Tests
+namespace NpvCalculator.Tests.Application
 {
     public class NpvCalculationTests
     {
         [Theory]
-        [InlineData(1000, 0.1, new[] { 500.0, 500.0, 500.0 }, 243.4262)]
+        [InlineData(1000, 0.1, new[] { 500.0, 500.0, 500.0 }, 243.4260)]
         public void CalculateNpv_ValidInput_ReturnsCorrectValue(
-            decimal initialInvestment, 
-            double discountRate, 
+            decimal initialInvestment,
+            double discountRate,
             double[] cashFlows,
             decimal expected)
         {
             // Arrange
 
             // Act
-            var result = NpvCalculatorService.CalculateNpv(initialInvestment, 
-                                                            (decimal)discountRate, 
+            var result = NpvCalculatorService.CalculateNpv(initialInvestment,
+                                                            (decimal)discountRate,
                                                             cashFlows.Select(Convert.ToDecimal).ToList());
-            
+
             // Assert
             Assert.Equal(expected, result);
 
