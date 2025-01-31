c3317dc147d8ffb008a00c308f49ec1c703fbe91
domidomdomz <domz.ciriaco@gmail.com>
2025-01-31 09:25:38 +0800

    Create Validator for CalculateNpvCommand

---
+++ NpvCalculator.Application/Npv/Validators/CalculateNpvCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using NpvCalculator.Application.Npv.Commands;
+
+namespace NpvCalculator.Application.Npv.Validators
+{
+    public class CalculateNpvCommandValidator : AbstractValidator<CalculateNpvCommand>
+    {
+        public CalculateNpvCommandValidator() 
+        { 
+            RuleFor(x => x.InitialInvestment)
+                .GreaterThan(0)
+                .WithMessage("Initial investment must be positive");
+
+            RuleFor(x => x.DiscountRate)
+                .InclusiveBetween(0, 1)
+                .WithMessage("Discount rate must be between 0% and 100%");
+
+            RuleFor(x => x.CashFlows)
+                .NotEmpty()
+                .WithMessage("At least one cash flow required")
+                .Must(x => x.All(amount => amount > 0))
+                .WithMessage("All cash flows must be positive");
+        }
+    }
+}
